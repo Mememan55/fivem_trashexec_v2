66b5eaa0ade20521d296dd659fc820be203fed3c
Mememan55 <34114942+Mememan55@users.noreply.github.com>
2020-05-28 23:51:23 +0200

    yep cock

--- exec/exec/dllmain.cpp
+++ exec/exec/dllmain.cpp
@@ -1,42 +1,64 @@
-#include "pch.h"
-#include <cstdint>
-#include <stdio.h>
-#include <stdlib.h>
-
-using LoadSystemFile_call = __int64(*)(__int64 state, const char* scriptName);
-uint64_t luaClass = 0x0;
+#include <thread>
+#include "CustomAPI.h"
 
 
-auto grabClass() -> void {
-	while (true) {
-		Sleep(250);
-		uint64_t* c1 = (uint64_t*)(GetModuleHandleA("citizen-scripting-lua") + 0x4FECC8);
-		if (*c1 != 0)
-			luaClass = *c1;
-	}
+using RunFileInternal_t = int(*)(uint64_t, const char*, std::function<int(char*)>);
+using LoadSystemFileInternal_t = int(*)(uint64_t, const char*);
+char LSFIShell[] = { 0x55, 0x56, 0x57, 0x53, 0x48, 0x83, 0xEC, 0x38, 0xFF, 0x25, 0x00, 0x00, 0x00, 0x00, 0x69, 0x69, 0x69, 0x69, 0x69, 0x69, 0x69, 0x69 };
+
+uint64_t csLuaBase;
+uint64_t grabbedInstance;
+uint64_t allocLSFI;
+
+
+int _fastcall LoadSystemFileInternal(uint64_t luaRuntime, const char* scriptFile) {
+    if (!allocLSFI) {
+        allocLSFI = reinterpret_cast<uint64_t>(VirtualAlloc(NULL, sizeof(LSFIShell), MEM_COMMIT, 0x40));
+        memcpy((void*)allocLSFI, (void*)LSFIShell, sizeof(LSFIShell));
+        *(uint64_t*)(allocLSFI + 14) = csLuaBase + 0x27998;
+    }
+
+    return ((LoadSystemFileInternal_t)(allocLSFI))(luaRuntime, scriptFile);
 }
 
-auto doStuff() -> void {
-	CreateThread(0, 0, (LPTHREAD_START_ROUTINE)grabClass, 0, 0, 0);
-	
-	LoadSystemFile_call lsfC = (LoadSystemFile_call)(GetModuleHandleA("citizen-scripting-lua") + 0x50D50);
-
-	printf("Press F5 to execute C:/nigger.lua\n");
-	while (true) {
-		if (GetAsyncKeyState(VK_F5)) {
-			lsfC(luaClass, "C:\\nigger.lua");
-		}
-	}
-
+int LoadSystemFile(uint64_t luaRuntime, const char* scriptFile) {
+    *(BYTE*)(CustomAPI::GetModuleA("adhesive") + 0x471458) = 1;
+    auto result = ((RunFileInternal_t)(csLuaBase + 0x27A80))(luaRuntime, scriptFile, std::bind(&LoadSystemFileInternal, luaRuntime, std::placeholders::_1));
+    return result;
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
-    if(ul_reason_for_call == DLL_PROCESS_ATTACH)
-        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)doStuff, 0, 0, 0);
+
+BOOL APIENTRY DllMain( HMODULE hModule, DWORD  callReason, LPVOID lpReserved ){
+    if (callReason == DLL_PROCESS_ATTACH) {
+        std::thread([&] {
+            while (!csLuaBase)
+                csLuaBase = CustomAPI::GetModuleA("citizen-scripting-lua");
+            
+            for (;;) {
+                uint64_t* c1 = (uint64_t*)(csLuaBase + 0x60CEF0);
+                if (*c1 != 0)
+                    grabbedInstance = *c1;
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(250));
+            }
+        }).detach();
+
+        std::thread([&] {
+
+            for (;;) {
+                if (!grabbedInstance)
+                    std::this_thread::sleep_for(std::chrono::seconds(5));
+
+                if (GetAsyncKeyState(VK_F5)) {
+                    LoadSystemFile(grabbedInstance, "C:/memes/test.lua");
+                    std::this_thread::sleep_for(std::chrono::seconds(2));
+                }
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            }
+
+        }).detach();
+    }
 
     return TRUE;
 }
