a190f2e2db5b87e25703995620c6d6c223ed9cdb
Mememan55 <34114942+Mememan55@users.noreply.github.com>
2020-05-29 00:16:37 +0200

    f

--- exec/exec/dllmain.cpp
+++ exec/exec/dllmain.cpp
@@ -54,7 +54,7 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  callReason, LPVOID lpReserved ){
                     std::this_thread::sleep_for(std::chrono::seconds(2));
                 }
 
-                std::this_thread::sleep_for(std::chrono::milliseconds(200));
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
             }
 
         }).detach();
